5f94f95e7ba71594d3e87afca742ee31c0aa8a45
Christian Stewart <kidovate@gmail.com>
2014-11-03 18:55:34 -0800

    Update to latest Meteor version format

--- package.js
+++ package.js
@@ -1,16 +1,21 @@
 Package.describe({
-  summary: "Login service for Steam accounts"
+    name: "kidovate:accounts-steam",
+    summary: "Login service for Steam.",
+    version: '1.0.0',
+    git: 'https://github.com/paralin/accounts-steam.git'
 });
 
 Package.on_use(function(api) {
-  api.use(['underscore', 'random']);
-  api.use('accounts-base', ['client', 'server']);
-  // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('steam', ['client', 'server']);
+    api.versionsFrom('1.0');
 
-  api.add_files('steam_login_button.css', 'client');
+    api.use(['underscore', 'random']);
+    api.use('accounts-base', ['client', 'server']);
+    // Export Accounts (etc) to packages using this one.
+    api.imply('accounts-base', ['client', 'server']);
+    api.use('accounts-oauth', ['client', 'server']);
+    api.use('steam', ['client', 'server']);
 
-  api.add_files("steam.js");
+    api.add_files('steam_login_button.css', 'client');
+
+    api.add_files("steam.js");
 });
