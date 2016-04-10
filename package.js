2b35b9db58ef3105edb983833b1cd0ed8eeef1c8
Christian Stewart <christian@paral.in>
2016-04-10 15:38:10 -0400

    Release new version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "kidovate:accounts-steam",
     summary: "Login service for Steam.",
-    version: '1.0.1',
+    version: '1.0.2',
     git: 'https://github.com/paralin/accounts-steam.git',
     documentation: null
 });
